ecea0a800a52a9ceebeb093d6dfdd9610e1ef966
ryanarnab <ryanghosh280@gmail.com>
2026-09-21 01:01:32 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Instrument_Serif } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +32,8 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-black text-white">
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
